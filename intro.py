0facaf0f2315e0fddda124d244a3fb0dd9fe5ddc
brainschild <brainschild.bc@gmail.com>
2019-03-23 16:59:25 +0300

    Update intro.py

--- intro.py
+++ intro.py
@@ -7,7 +7,7 @@ print(message[7:12]) #prints the instance of the string from index 7 to index 12
 
 print(message.lower()) #print string in lowercase
 print(message.upper()) #prints String in UPPERCASE
-print(message[0],message.count('b')) #takes the index character at 0 , counts how many instaces of M are n the string.
+print(message[0],message.count('b')) #takes the index character at [0] , counts how many instaces of M are n the string.
 print(message[1],message.count('r'))
 print(message[2],message.count('a'))
 print(message[3],message.count('i'))
@@ -22,7 +22,7 @@ print(replace_message)
 greeting = 'Hello'
 name = 'Benta'
 #appends both String to form a Sentence/String... there is use of (+) and String spaces and commas. Gets complicated with more inclusions of Strings
-greetme = greeting +', '+ name+'. Welcome! NOT'
+greetme = greeting +', '+ name+'. Welcome! using plus signs to add combine the Strings'
 #second instance uses placeholders thus instigates the consruction of normal instance of how the senstence
 #would appear then implores the use of .format which is feed with the Strings
 greetme2 = '{}, {}. Welcome!'.format(greeting,name)
