49ff11d7db0bfea9acc2124abc93cf883af2cc9f
brainschild <brainschild.bc@gmail.com>
2019-03-23 17:31:40 +0300

    Add files via upload

---
+++ python/print(help(str.lower)).py
@@ -0,0 +1,10 @@
+print(help(str.lower))
+##### Result #####
+
+'''Help on method_descriptor:
+
+lower(self, /)
+    Return a copy of the string converted to lowercase.
+
+None
+[Finished in 0.1s]'''
